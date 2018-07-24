d992c41516a7fccbacc9038ead514e6d0f4f7938
Wilson Silva <wilsonsilva80@gmail.com>
2018-07-24 19:31:31 +0100

    Book component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
+    state = {
+        books: [],
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+        showSearchPage: false
+    }
+
+  componentDidMount() {
+      BooksAPI.getAll().then((books) =>{
+          console.log(books)
+          this.setState({ books })
+      })
   }
 
   render() {
     return (
       <div className="app">
+
         {this.state.showSearchPage ? (
           <div className="search-books">
             <div className="search-books-bar">
@@ -38,6 +47,7 @@ class BooksApp extends React.Component {
             </div>
           </div>
         ) : (
+
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
---
+++ src/Book.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Book extends Component {
+    static propTypes = {
+        book: PropTypes.object.isRequired,
+        onShelf: PropTypes.func.isRequired
+    }
+
+    // onShelf: function with param book and shelf
+    handleChange = (e) => {
+        onShelf(book, e.target.value)
+    }
+
+
+    render() {
+        const { book, shelf } = this.props
+
+        return (
+            <li>
+                <div className="book">
+                    <div className="book-top">
+                        <div
+                            className="book-cover"
+                            style={ {
+                                    width: 128,
+                                    height: 193,
+                                    backgroundImage: `url("${book.imageLinks.thumbnail}")`
+                                } } ></div>
+                        <div className="book-shelf-changer">
+                            <select value={book.shelf} onChange={this.handleChange}>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors}</div> // TODO: check if > 1
+                </div>
+            </li>
+        )
+    }
+}
+
+export default Book
